7a574c421e2afc92beaf0a977a91d21fcf2289bb
AndriyBilous <isnogood@ukr.net>
2022-11-08 12:20:29 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c7f046b9-cd47-4088-8985-a598d8a6bd01';
+export const MODAL_CLOSE_SEC = 2.5;
